c3e92529ccacfe5b524cb54dafe112b6539983ad
Johnbiniok <57683634+Johnbiniok@users.noreply.github.com>
2020-10-26 20:26:43 -0500

    Initial check-in of module BMIcalculator

---
+++ BMIcalculator/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BMIcalculator
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        int count = 0;
+        void Handle_Submit(object sender, System.EventArgs e)
+        {
+            count++;
+            var weightF = double.Parse(weight.Text);
+            var feetNum = double.Parse(feet.Text);
+            var inchNum = double.Parse(inches.Text);
+
+            var kilograms = weightF / 2.20462;
+            var ftmeters = feetNum * 0.3048;
+            var inMeters = inchNum * 0.0254;
+            var meters = ftmeters + inMeters;
+            var msquared = meters * meters;
+            var BMI = kilograms / msquared;
+
+            BMIout.Text = $"Your BMI is {BMI}";
+            //((Button)sender).Text = $"You clicked {count} times.";
+        }
+    }
+}
